144861cf3d6cd7e06b6239794dca2241ec319608
Tamara Chance <tamara.n.chance@gmail.com>
2021-10-27 16:30:09 -0500

    part 2

---
+++ index.js
@@ -0,0 +1,24 @@
+//console.log("hello world");
+
+//Callback function
+let addMovie = (event) => {
+    event.preventDefault();
+    let inputField = document.querySelector('input')
+    let movie = document.createElement('li');
+    let movieTitle = document.createElement('span');
+
+    movieTitle.textContent = inputField.value;
+    movie.appendChild(movieTitle);
+    ul.appendChild(movie);
+
+}
+
+
+
+
+let ul = document.querySelector('ul');
+
+let form = document.querySelector('form');
+
+//Calling the function upon submit
+form.addEventListener('submit', addMovie);
